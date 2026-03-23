6746a984b8dfb1f2730ee330f30a4814ba347227
Epergande <elliott.pergande1@gmail.com>
2026-03-23 08:01:34 -0500

    other changes

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor.Compilation;
 using Microsoft.EntityFrameworkCore;
+
+using Microsoft.Extensions.Configuration;
 
 namespace Northwind.Controllers
 {
@@ -8,6 +11,51 @@ namespace Northwind.Controllers
      // this controller depends on the DataContext
         private readonly DataContext _dataContext = db;
 
-        // public IActionResult Regester() => View(_dataContext.Customers);
+
+[HttpGet]
+
+        public IActionResult Regester() { 
+            return View("Regester");
+        }
+  [HttpGet]
+public IActionResult Customers()
+{
+    // Fetch the list of customers from the database
+    var model = _dataContext.Customers.ToList();
+    
+    // This will look for the "Customers.cshtml" view
+    return View(model);
+}
+        
+
+    [HttpPost]
+    public IActionResult Regester(string companyName , string address , string city,
+ string region,
+ string postalCode,
+ string country,
+ string phone,
+ string fax)
+    {
+         if (db.Customers.Any(c => c.CompanyName == companyName))
+      {
+        Console.WriteLine("error");
+        return View("Regester");
+      }
+      else
+      {
+        db.AddCustomer(companyName,address,city,
+          region,
+          postalCode,
+          country,
+          phone,
+          fax
+  );
+   return RedirectToAction("Customers"); // Redirect after success
+      }
+     
+
+       
     }
+    }
+
 }
--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -9,4 +9,32 @@ public class DataContext : DbContext
   public DbSet<Category> Categories { get; set; }
 
   public DbSet<Discount> Discounts { get; set; }
+
+  public DbSet<Customer> Customers { get; set;  } 
+
+public void AddCustomer(
+  string companyName ,
+ string address,
+ string city,
+ string region,
+ string postalCode,
+ string country,
+ string phone,
+ string fax
+
+ )
+    {
+        var newCustomer = new Customer { 
+          CompanyName = companyName ,
+          Address = address ,
+          City = city,
+          Region = region,
+          PostalCode = postalCode,
+          Country = country,
+          Phone = phone,
+          Fax = fax
+        };
+        this.Customers.Add(newCustomer);
+        this.SaveChanges();
+    }
 }
