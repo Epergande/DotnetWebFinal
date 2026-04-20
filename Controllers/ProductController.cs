00175f356e7eaffaeccafb99a2b03263e0b28c05
Jacob Tunnicliff <jtunnicliff@my.wctc.edu>
2026-04-20 10:03:51 -0500

    ProductControoler add discount method created, not tested if it works or not

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 public class ProductController : Controller
 {
@@ -6,11 +7,33 @@ public class ProductController : Controller
   private DataContext _dataContext;
   public ProductController(DataContext db) => _dataContext = db;
   public IActionResult Category() => View(_dataContext.Categories.OrderBy(c => c.CategoryName));
-  public IActionResult Index(int id){
+  public IActionResult Index(int id)
+  {
     ViewBag.id = id;
     return View(_dataContext.Categories.OrderBy(c => c.CategoryName));
   }
-public IActionResult Discount(int id) => View(_dataContext.Discounts);
+  public IActionResult Discount(int id) => View(_dataContext.Discounts);
 
-public IActionResult AddDiscount() => View();
+
+  [Authorize(Roles = "employee")]
+  public IActionResult AddDiscount() => View();
+  [Authorize(Roles = "employee")]
+  [HttpPost]
+  [ValidateAntiForgeryToken]
+  public IActionResult AddDiscount(Discount model)
+  {
+    if (ModelState.IsValid)
+    {
+      if (_dataContext.Discounts.Any(b => b.DiscountId == model.DiscountId))
+      {
+        ModelState.AddModelError("", "Name must be unique");
+      }
+      else
+      {
+        _dataContext.AddDiscount(model);
+        return RedirectToAction("Index");
+      }
+    }
+    return View();
+  }
 }
