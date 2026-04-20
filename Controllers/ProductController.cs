e34e896adc17ae6d975d079b2554d7207a48fc88
Epergande <elliott.pergande1@gmail.com>
2026-04-20 09:50:04 -0500

    somethin

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class ProductController : Controller
+{
+  // this controller depends on the NorthwindRepository
+  private DataContext _dataContext;
+  public ProductController(DataContext db) => _dataContext = db;
+  public IActionResult Category() => View(_dataContext.Categories.OrderBy(c => c.CategoryName));
+  public IActionResult Index(int id){
+    ViewBag.id = id;
+    return View(_dataContext.Categories.OrderBy(c => c.CategoryName));
+  }
+public IActionResult Discount(int id) => View(_dataContext.Discounts);
+
+public IActionResult AddDiscount() => View();
+}
---
+++ Models/DataContext.cs
@@ -0,0 +1,52 @@
+using Microsoft.EntityFrameworkCore;
+
+public class DataContext : DbContext
+{
+  public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+  public DbSet<Product> Products { get; set; }
+  public DbSet<Category> Categories { get; set; }
+  public DbSet<Discount> Discounts { get; set; }
+  public DbSet<Customer> Customers { get; set; }
+
+  public void AddCustomer(Customer customer)
+  {
+    Customers.Add(customer);
+    SaveChanges();
+  }
+  public void EditCustomer(Customer customer)
+  {
+    var customerToUpdate = Customers.FirstOrDefault(c => c.CustomerId == customer.CustomerId);
+    customerToUpdate.Address = customer.Address;
+    customerToUpdate.City = customer.City;
+    customerToUpdate.Region = customer.Region;
+    customerToUpdate.PostalCode = customer.PostalCode;
+    customerToUpdate.Country = customer.Country;
+    customerToUpdate.Phone = customer.Phone;
+    customerToUpdate.Fax = customer.Fax;
+    SaveChanges();
+  }
+   public void AddDiscount(Discount discount)
+  {
+    this.Add(discount);
+    this.SaveChanges();
+  }
+  public void DeleteDiscount(Discount discount)
+  {
+    this.Remove(discount);
+    this.SaveChanges();
+  }
+  public void EditDiscount(Discount discount)
+  {
+    var discountToUpdate = Discounts.FirstOrDefault(c => c.DiscountId == discount.DiscountId);
+    discountToUpdate.Code = discount.Code;
+    discountToUpdate.StartTime = discount.StartTime;
+    discountToUpdate.EndTime = discount.EndTime;
+    discountToUpdate.ProductId = discount.ProductId;
+    discountToUpdate.DiscountPercent = discount.DiscountPercent;
+    discountToUpdate.Title = discount.Title;
+    discountToUpdate.Description = discount.Description;
+    discountToUpdate.Product = discount.Product;
+    
+  }
+}
