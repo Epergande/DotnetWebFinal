561998e78dad50d1492764b95c473e90731e6aaa
Epergande <elliott.pergande1@gmail.com>
2026-04-20 09:35:06 -0500

    Added Discount Form (Unfinished)

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -10,5 +10,7 @@ public class ProductController : Controller
   public IActionResult Category() => View(_dataContext.Categories.OrderBy(c => c.CategoryName));
   public IActionResult Index(int id) => View(_dataContext.Products.Where(p => p.CategoryId == id && p.Discontinued == false).OrderBy(p => p.ProductName));
 public IActionResult Discount(int id) => View(_dataContext.Discounts);
+
+public IActionResult AddDiscount() => View();
 }
 // .Where(p => p.EndTime !< thisDay)
